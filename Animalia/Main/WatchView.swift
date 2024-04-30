dc23089a7e3dc920facfff72dd5ad1aa7b9785d3
Jess <jessen2323@gmail.com>
2024-04-30 10:28:03 +0700

    Implementing videos feature in Watch

---
+++ Animalia/Main/WatchView.swift
@@ -0,0 +1,18 @@
+//
+//  WatchView.swift
+//  Animalia
+//
+//  Created by User50 on 30/04/24.
+//
+
+import SwiftUI
+
+struct WatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchView()
+}
---
+++ Animalia/Modules/Screen/Watch/VideoListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListItemView.swift
+//  Animalia
+//
+//  Created by User50 on 30/04/24.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoListItemView()
+}
